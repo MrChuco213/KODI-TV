934b2f36655884556ee24b5cab7abf51a111f998
MrChuco213 <macossierra@macOS-Sierra.local>
2016-08-17 03:17:59 -0700

    Initial Commit

---
+++ KODI-TV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KODI-TV
+//
+//  Created by Vicente Gutierrez on 8/14/16.
+//  Copyright © 2016 Vicente Gutierrez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
